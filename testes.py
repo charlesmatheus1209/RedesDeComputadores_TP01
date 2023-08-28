aec40431c51149182fc15af5dac13c0997db0c19
Marielle Cristina <98425117+MarielleCosta@users.noreply.github.com>
2023-08-28 19:57:05 -0300

    Desencapsula

--- testes.py
+++ testes.py
@@ -19,8 +19,22 @@ def VerificaChecksum(informacao, ValorChecksum):
         # print('A informação não chegou perfeitamente')
         return False
 
+#Todas os parâmetros vão estar corretos e e como string
+def EncapsulamentoQuadro(mensagem,controle,pacote,cksum_quadro):
+    separador ="$%#$%#"
+    quadro = ""
+    quadro = '[' + controle + separador + pacote + separador + mensagem + separador + str(cksum_quadro) + ']'
+    return quadro
+
+def DesencapsulaQuadro(quadro):
+    quadro_recuperado = quadro.replace("$%#$%#","")
+    return quadro_recuperado
+
 # Checksum('Arroz')
 
-print(VerificaChecksum('Arroz', Checksum('Arroz')))
-print("Hello World")
+# print(VerificaChecksum('Arroz', Checksum('Arroz')))
+# print("Hello World")
+
+print(EncapsulamentoQuadro("OIMUNDO","D","3","265"))
+print(DesencapsulaQuadro(EncapsulamentoQuadro("OIMUNDO","D","3","265")))
 
